0380e4f397a7b831c21c439fd8d1bd2d3e3aaff7
Itamar <itamar.schnarch@gmail.com>
2025-01-16 22:26:45 +0200

    feat: add basic temp constants class

---
+++ OmegaSudoku/Models/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaSudoku.Models
+{
+    public static class Constants
+    {
+
+        public const int BoardSize = 9;
+        public const int MinCellValue = 1;
+        public const int MaxCellValue = BoardSize;
+    }
+
+}
