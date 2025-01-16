dccc5608c0a2df10889c132bcdbe6782b3ef3196
Itamar <itamar.schnarch@gmail.com>
2025-01-16 20:54:15 +0200

    feat: add basic temp input handlers

---
+++ OmegaSudoku/Services/Input/CliInputHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaSudoku.Services.Input
+{
+    public class CliInputHandler : IInputHandler
+    {
+        public string GetInput()
+        {
+            Console.WriteLine("Enter 9x9 sudoku board");
+            String input = Console.ReadLine();
+
+            return input;
+        }
+    }
+}
---
+++ OmegaSudoku/Services/Input/IInputHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaSudoku.Services.Input
+{
+    public interface IInputHandler
+    {
+        string GetInput();
+    }
+}
