8db4500345b19520511ec436162bb2d1b077c821
Itamar <itamar.schnarch@gmail.com>
2025-01-20 13:28:09 +0200

    refactor: change the main function for temporary classes testing

--- OmegaSudoku/Program.cs
+++ OmegaSudoku/Program.cs
@@ -1,4 +1,5 @@
-﻿using OmegaSudoku.Models;
+﻿using OmegaSudoku.Logic;
+using OmegaSudoku.Models;
 using OmegaSudoku.Services.Input;
 using OmegaSudoku.Services.Output;
 
@@ -10,9 +11,15 @@ public class Program
 
         CliInputHandler inputHandler = new CliInputHandler();
         CliOutputHandler outputHandler = new CliOutputHandler();
-        BoardFormatter boardFormatter = new BoardFormatter();
-        //outputHandler.printBoard(inputHandler.GetInput());
-        inputHandler.IsInputValidSudoku("235235257a2350mgn25ho0980,");
+
+        SudokuBoard board = new SudokuBoard(9, "100000027000304015500170683430962001900007256006810000040600030012043500058001000");
+        outputHandler.PrintBoard(board);
+        SudokuSolver sudokuSolver = new SudokuSolver(board);
+        bool flag = sudokuSolver.Solve();
+        board = sudokuSolver.GetSudokuBoard();
+        outputHandler.PrintBoard(board);
+
+
 
 
     }
