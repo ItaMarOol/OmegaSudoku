263d512a7cab7b435bbc3ad61c9c51ea19957b7d
Itamar <itamar.schnarch@gmail.com>
2025-01-25 20:17:19 +0200

    feat: add basic custom exceptions

---
+++ OmegaSudoku/Exceptions/DuplicateValueException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaSudoku.Exceptions
+{
+    class DuplicateValueException : Exception
+    {
+        public DuplicateValueException(int duplicatedValue)
+            : base($"Duplicated value in the same row/column/block entered: {duplicatedValue}")
+        { }
+    }
+}
---
+++ OmegaSudoku/Exceptions/InvalidBoardSizeException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaSudoku.Exceptions
+{
+    class InvalidBoardSizeException : Exception
+    {
+        public InvalidBoardSizeException(int invalidBoardSize)
+            : base($"Invalid board size entered: {invalidBoardSize}")
+        { }
+    }
+}
